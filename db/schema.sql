eefe8a139a44a1c712a305c637a4c0f281ac2d4e
Potwora <matthewpotwora@yahoo.com>
2022-10-18 19:25:38 -0700

    worked on and fixed the inquirer prompts and questioning

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,29 @@
-CREATE TABLE  employees(
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+DROP DATABASE IF EXISTS employeeBusinessDB;
+CREATE DATABASE employeeBusinessDB;
+USE employeeBusinessDB;
+
+-- Department table
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+-- Department table
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
+-- Employee role table
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30)NOT NULL,
+  last_name VARCHAR(30)NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,31 @@
-INSERT INTO employees (first_name, last_name, industry_connected)
-VALUES
-  ('Ronald', 'Firbank', 1),
-  ('Virginia', 'Woolf', 1),
-  ('Piers', 'Gaveston', 0),
-  ('Charles', 'LeRoi', 1),
-  ('Katherine', 'Mansfield', 1),
-  ('Dora', 'Carrington', 0),
-  ('Edward', 'Bellamy', 0),
-  ('Montague', 'Summers', 1),
-  ('Octavia', 'Butler', 1),
-  ('Unica', 'Zurn', 1);
+-- Department seeds
+INSERT INTO departments (id, name)
+VALUES (1, "Board"),
+       (2, "Sales"),
+       (3, "Engineering"),
+       (4, "Finance"),
+       (5, "Legal");
+
+-- Role seeds
+INSERT INTO roles (department_id, title, salary)
+VALUES (1, "CEO", 1000000),
+       (2, "Sales Lead", 100000),
+       (2, "Salesperson", 80000),
+       (3, "Lead Engineer", 150000),
+       (3, "Software Engineer", 120000),
+       (4, "Accountant Manager", 160000),
+       (4, "Accountant", 125000),
+       (5, "Legal Team Lead", 250000),
+       (5, "Lawyer", 190000);
+
+-- Employee seeds
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Matt", "Potwora", 1, null),
+       ("John", "Doe", 2, 1),
+       ("Mike", "Chan", 3, 3),
+       ("Ashley", "Rodriguez", 4, 1),
+       ("Kevin", "Tupik", 5, 4), 
+       ("Anthony", "Singh", 6, 1),
+       ("Malia", "Brown", 7, 5),
+       ("Sarah", "Lourd", 8, 1),
+       ("Tom", "Allen", 9, 6);
